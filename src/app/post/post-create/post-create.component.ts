d7d6f92ac16e8dd9cc0ff88f40b0c6d93ab6a62c
RuanSena <ruansenadev@gmail.com>
2020-06-22 16:54:20 -0300

    binding

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
 })
 
 export class PostCreateComponent {
+  content = 'no text';
 
+
+  onAddPost() {
+    this.content = 'ola ganhafoto, o quanto ja cresceu?'
+  }
 }
